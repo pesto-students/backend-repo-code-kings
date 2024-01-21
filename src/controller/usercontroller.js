5fb0453ae468cae77ccb671b6e1d330d322db3fa
arpitmoliya <arpitmoliya153@gmail.com>
2024-01-21 11:40:57 +0530

    feat: basic folder stucture files

---
+++ src/controller/usercontroller.js
@@ -0,0 +1,13 @@
+const User = require("../models/user");
+
+exports.createUser = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const newUser = new User({ name, email });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
