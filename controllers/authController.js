57ab9385ddfe6f15a306898fa546a810965f8752
asyncasad <asyncasad@gmail.com>
2024-01-30 08:54:27 +0530

    cookie setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,10 +9,20 @@ const signToken = (id) => {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
-
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   user.password = undefined;
+  // COOKIE OPTIONS OBJECT
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  // SETTING SECURE CONDITION
+  if (process.env.NODE_ENV === "PRODUCTION") cookieOptions.secure = true;
+  // SETTING COOKIE TOKEN
+  res.cookie("jwt", token, cookieOptions);
   res.status(statusCode).json({
     status: "success",
     token,
