ccb71adcb90411834ef0b068fef58b50b37ff822
asyncasad <asyncasad@gmail.com>
2024-01-30 10:06:23 +0530

    add ratelimiting sanitization and prevent parameter poullution

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const mongoSanitize = require("express-mongo-sanitize");
 const AppError = require("./utils/appError");
 const userRouter = require("./routes/userRoutes");
 const globalErrorController = require("./controllers/errorController");
+// ADD LIMITER
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api/", limiter);
+// HELMET
+app.use(helmet());
 // BODY PARSER
 app.use(express.json());
+// MONGO SANITIZE
+app.use(mongoSanitize());
+// SANITISATON AGAINS NOSQL INJECTION AND XSS
+app.use(xss());
+// PARAMETER POLUTION
+app.use(hpp());
 // DEFINED ROUTES
 app.use("/api/v1/users", userRouter);
 // UNDEFINED ROUTES HANDLER
