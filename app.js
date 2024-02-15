8b8a7bcfb8bda17c67941cdf9bd3df1368afb8ed
arpitmoliya <arpitmoliya153@gmail.com>
2024-02-15 22:21:22 +0530

    Revert "fix: cors"
    This reverts commit ef6e930646015701f7ede119291b20a2e6a77a71.

--- app.js
+++ app.js
@@ -20,11 +20,10 @@ const limiter = rateLimit({
 app.use("/api/", limiter);
 const cors = require("cors");
 app.use(
-  cors()
-  //   {
-  //   credentials: true,
-  //   origin: ["http://localhost:3001", "https://energia-app.vercel.app/"],
-  // }
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3001", "https://energia-app.vercel.app/"],
+  })
 );
 // HELMET
 app.use(helmet());
